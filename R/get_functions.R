79530badfff572d1c7fc92c4a060f2ee7b4dff2c
vboyce <vero.boyce@gmail.com>
2025-07-02 16:53:07 -0700

    some joins work, image join not working

--- R/get_functions.R
+++ R/get_functions.R
@@ -9,6 +9,12 @@ refbank <- function(version) {
   redivis::redivis$user("mcfrank")$dataset("refbank", version = version)
 }
 
+table_keys <- c("datasets"="datasets:q7yy", "messages"="messages:2q18",
+                "trials"="trials:zkj2", "choices"="choices:s1zj",
+                "conditions"="conditions:kk1e", "players"="players:7xnd",
+                "images"="images:jw0t"
+)
+
 # run query on dataset and retrieve results
 get_dataset_query <- function(dataset, query_str, max_results) {
   dataset$query(query_str)$to_tibble(max_results = max_results)
@@ -27,20 +33,12 @@ build_filter <- function(var, vals) {
 
 # build a SQL query that subsets a dataset table by a vector of dataset IDs
 build_dataset_query <- function(primary_table, aux_joins, datasets, max_results) {
+  if (!is.null(max_results)) {max_results_str= glue("LIMIT {max_results}")}
+  else {max_results_str=""}
   dataset_filter <- build_filter(var = "dataset_id", vals = datasets)
-  glue("SELECT * FROM {table_keys[primary_table]} {dataset_filter} LIMIT {max_results}
-       {aux_joins}" ) |>
+   glue("SELECT * FROM {table_keys[primary_table]} {aux_joins} {dataset_filter} {max_results_str}" ) |>
     stringr::str_trim()
 }
-
-
-table_keys <- c("datasets"="datasets:q7yy", "messages"="messages:2q18",
-                "trials"="trials:zkj2", "choices"="choices:s1zj",
-                "conditions"="conditions:kk1e", "players"="players:7xnd",
-                "images"="images:jw0t"
-                )
-
-table_keys["datasets"]
 
 
 #' Get datasets
@@ -54,17 +52,14 @@ get_datasets <- function(version = "current", max_results = NULL) {
   get_dataset_table(refbank(version), "datasets:q7yy", max_results)
 }
 
-join_condition_string <- "LEFT JOIN {table_keys['conditions']}
-ON {table_keys['trials']}.condition_id = {table_keys['conditions']}.condition_id"
+join_conditions_string <- "LEFT JOIN {table_keys['conditions']} USING (condition_id, dataset_id)"
+#join_conditions_string <- "LEFT JOIN {table_keys['conditions']} ON {table_keys['trials']}.condition_id = {table_keys['conditions']}.condition_id"
 
-join_images_string <- "LEFT JOIN {table_keys['images']}
-ON {table_keys['trials']}.target = {table_keys['images']}.image_id"
+join_images_string <- "LEFT JOIN {table_keys['images']} ON {table_keys['trials']}.target = {table_keys['images']}.image_id"
 
-join_players_string <- "LEFT JOIN {table_keys['players']}
-ON {table_keys[primary_table]}.player_id = {table_keys['players']}.player_id"
+join_players_string <- "LEFT JOIN {table_keys['players']} USING (player_id, dataset_id)"
 
-join_trials_string <- "LEFT JOIN {table_keys['trials']}
-ON {table_keys[primary_table]}.trial_id = {table_keys['trials']}.trial_id"
+join_trials_string <- "LEFT JOIN {table_keys['trials']} USING (trial_id, dataset_id)"
 
 #' Get messages
 #'
@@ -81,12 +76,12 @@ get_messages <- function(version = "current", datasets = NULL, max_results = NUL
   primary_table="messages"
   join_string <- ""
   if (include_image_data || include_condition_data) {include_trial_data=T}
-  if (include_trial_data) {join_string=str_c(join_string, glue(join_trials_string))}
-  if (include_player_data) {join_string=str_c(join_string, glue(join_players_string))}
-  if (include_images_data) {join_string=str_c(join_string, glue(join_images_string))}
-  if (include_conditions_data) {join_string=str_c(join_string, glue(join_conditions_string))}
-
+  if (include_trial_data) {join_string=stringr::str_c(join_string, glue(join_trials_string))}
+  if (include_player_data) {join_string=stringr::str_c(join_string, glue(join_players_string))}
+  if (include_image_data) {join_string=stringr::str_c(join_string, glue(join_images_string))}
+  if (include_condition_data) {join_string=stringr::str_c(join_string, glue(join_conditions_string))}
   query_str <- build_dataset_query(primary_table, join_string, datasets, max_results)
+  message(query_str)
   get_dataset_query(refbank(version), query_str, max_results)
 }
 
@@ -100,9 +95,11 @@ get_trials <- function(version = "current", datasets = NULL, max_results = NULL,
                        include_image_data=F,
                        include_condition_data=F) {
 
-  if (include_images_data) {join_string=str_c(join_string, glue(join_images_string))}
-  if (include_conditions_data) {join_string=str_c(join_string, glue(join_conditions_string))}
-  query_str <- build_dataset_query("trials", datasets, max_results)
+  join_string=""
+  if (include_image_data) {join_string=stringr::str_c(join_string, glue(join_images_string))}
+  if (include_condition_data) {join_string=stringr::str_c(join_string, glue(join_conditions_string))}
+  query_str <- build_dataset_query("trials", join_string,  datasets, max_results)
+  message(query_str)
   get_dataset_query(refbank(version), query_str, max_results)
 }
 
@@ -119,11 +116,12 @@ get_choices <- function(version = "current", datasets = NULL, max_results = NULL
                         include_condition_data=F) {
 
   primary_table="choices"
+  join_string=""
   if (include_image_data || include_condition_data) {include_trial_data=T}
-  if (include_trial_data) {join_string=str_c(join_string, glue(join_trials_string))}
-  if (include_player_data) {join_string=str_c(join_string, glue(join_players_string))}
-  if (include_images_data) {join_string=str_c(join_string, glue(join_images_string))}
-  if (include_conditions_data) {join_string=str_c(join_string, glue(join_conditions_string))}
+  if (include_trial_data) {join_string=stringr::str_c(join_string, glue(join_trials_string))}
+  if (include_player_data) {join_string=stringr::str_c(join_string, glue(join_players_string))}
+  if (include_image_data) {join_string=stringr::str_c(join_string, glue(join_images_string))}
+  if (include_condition_data) {join_string=stringr::str_c(join_string, glue(join_conditions_string))}
   query_str <- build_dataset_query(primary_table, join_string, datasets, max_results)
   get_dataset_query(refbank(version), query_str, max_results)
 }
@@ -135,7 +133,7 @@ get_choices <- function(version = "current", datasets = NULL, max_results = NULL
 #'
 #' @export
 get_conditions <- function(version = "current", datasets = NULL, max_results = NULL) {
-  query_str <- build_dataset_query("conditions", datasets, max_results)
+  query_str <- build_dataset_query("conditions", "", datasets, max_results)
   get_dataset_query(refbank(version), query_str, max_results)
 }
 
@@ -145,7 +143,7 @@ get_conditions <- function(version = "current", datasets = NULL, max_results = N
 #'
 #' @export
 get_players <- function(version = "current", datasets = NULL, max_results = NULL) {
-  query_str <- build_dataset_query("players", datasets, max_results)
+  query_str <- build_dataset_query("players", "", datasets, max_results)
   get_dataset_query(refbank(version), query_str, max_results)
 }
 
@@ -155,6 +153,6 @@ get_players <- function(version = "current", datasets = NULL, max_results = NULL
 #'
 #' @export
 get_images <- function(version = "current", datasets = NULL, max_results = NULL) {
-  query_str <- build_dataset_query("images", datasets, max_results)
+  query_str <- build_dataset_query("images","",  datasets, max_results)
   get_dataset_query(refbank(version), query_str, max_results)
 }
